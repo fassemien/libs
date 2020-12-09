7fbec3e618f4f1c6eb2cd65bc7748c1f98496b7f
Fabrice ASSEMIEN <asskroah@gmail.com>
2020-12-09 18:16:11 +0000

    update

--- src/main/java/com/example/multimodule/service/MyService.java
+++ src/main/java/com/example/multimodule/service/MyService.java
@@ -3,7 +3,7 @@ package com.example.multimodule.service;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
-@Service
+
 @EnableConfigurationProperties(ServiceProperties.class)
 public class MyService {
     private final ServiceProperties serviceProperties;
