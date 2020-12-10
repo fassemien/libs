a9e9b29fa4b56493a40feda76857482859c61968
Fabrice ASSEMIEN <asskroah@gmail.com>
2020-12-10 10:47:26 +0000

    fabrice

--- src/main/java/com/example/multimodule/service/MyService.java
+++ src/main/java/com/example/multimodule/service/MyService.java
@@ -1,11 +1,7 @@
 package com.example.multimodule.service;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Service;
 
-@Service
 public class MyService {
-
 
     public String message(){
         return "yes it'work";
